401468c9ddb489a6eba78c806a138a4767d58b2f
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-05-01 17:03:43 -0300

    exercicios de funções

--- aula1/script.js
+++ aula1/script.js
@@ -1,4 +1,56 @@
-var age = 61
+/* 
+function maiorDeIdade(idade) {
+    if (idade >= 18) {
+        return true
+    } else {
+        return false
+    }
+}
 
-let isAdult = age >= 18 && age < 60 ? 'Sim' : 'Não'
-console.log(isAdult)
+let idade = 11
+let verificação = maiorDeIdade(idade)
+
+if (verificação) {
+    console.log('É maior de idade')
+} else {
+    console.log('É menor de idade')
+} 
+*/
+
+/*
+function calcularImovel(metragem, quartos) {
+    let m2 = 3000
+    if (quartos == 1) {
+        let preco = 3000
+        return preco
+    } else if (quartos == 2) {
+        let preco = m2 * 1.2
+        return preco
+    } else if (quartos == 3) {
+        let preco = m2 * 1.5
+        return preco
+    }
+}
+*/
+
+function calcularImovel(metragem, quartos) {
+    let m2 = 3000
+    switch (preco) {
+        case 1: 
+            default: 
+                preco = metragem * m2
+                return preco
+        break
+        case 2: 
+            preco = metragem * m2
+        break
+        case 3:
+
+        break
+    }
+}
+
+let metragem = 123
+let quartos = 1
+let preco = calcularImovel(metragem, quartos)
+console.log(`A casa custa R$ ${preco}`)
