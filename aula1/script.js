8e0e607730f4a76a84ebee8bf27ecd6193bb4fb9
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-05-03 18:25:56 -0300

    aprendendo sobre objetos

--- aula1/script.js
+++ aula1/script.js
@@ -1,3 +1,10 @@
-let lista = ['blabla', ['xx', 'yy']]
-let segunda = lista[1]
-console.log(segunda[1])
+let personagem = {
+    nome: 'Vinicius',
+    idade: 16,
+    carros: [
+        {modelo: 'Fiat', cor: 'preto'},
+        {modelo: 'Ferrari', cor: 'vermelho'}
+    ]
+}
+
+console.log(personagem.carros[1].modelo)
