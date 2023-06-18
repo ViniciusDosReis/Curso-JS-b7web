49b01890ea68ec74050ec46d1463d73843c7fe88
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-06-18 12:41:39 -0300

    POO javascript

--- aula1/script.js
+++ aula1/script.js
@@ -1,7 +1,29 @@
+class Person {
 
-function soltou() {
-    
+    age = 0
+    steps = 0
+
+    constructor (name) {
+        this.name = name
+    }
+
+    takeAStep() {
+        this.steps++
+    }
+
+    setAge(newAge) {
+        if (typeof newAge == 'number') {
+            this.age = newAge
+        } else {
+            console.log('Idade não aceita. (Só números)')
+        }
+    }
 }
 
-const input = document.querySelector('input')
-input.addEventListener('keyup', soltou)
+let p1 = new Person('João')
+let p2 = new Person("Maria")
+let p3 = new Person("Pedro")
+
+p1.setAge(20)
+
+console.log(`${p1.name} tem ${p1.age} anos.`)
