75b830eba656e17527b179924a678b73130237d3
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-07-02 12:49:24 -0300

    operadores

--- aula1/script.js
+++ aula1/script.js
@@ -1,17 +1,52 @@
-class Person {
-    
-    static hands = 2
-    age = 0
+// Resolver As quatros operações
+let x = 15
+let y = 20
 
-    constructor(name) {
-        this.name = name
-    }
+calcular(x, y)
 
-    static sayHi() {
-        console.log()
-    }
-
+function calcular(a, b) {
+    console.log(a + b)
+    console.log(a - b)
+    console.log(a * b)
+    console.log(a / b)
 }
 
-let p1 = new Person("Bonieky")
+// A potência de um numero.
+let base = 3
+let expoente = 4
 
+function potencia(b, e) {
+    return base * base * base * base
+}
+
+let resul = potencia(base, expoente)
+console.log(resul)
+
+// Saber se um número é primo.
+let number = 4
+function isPrime(number) {
+    if (number <= 1) {
+        return false
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+let res = isPrime(number)
+
+if (res == true) {
+    console.log(`${number} é primo!`)
+} else {
+    console.log(`${number} não é primo!`)
+}
+
+let n = 10
+for (let i = 1; 1 < n; i++) {
+    if (n % i !== 0) {
+        console.log(i)
+    }
+}
