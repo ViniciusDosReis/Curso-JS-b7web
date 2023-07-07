e90121e5f933701d43589e5174d738d301112c5e
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-07-07 12:00:14 -0300

    arrows functions

--- aula1/script.js
+++ aula1/script.js
@@ -1,3 +1,8 @@
-function nomeCompleto (nome, sobrenome) {
-    
-}
+/*function sobrenome(sob) {
+    return 'Bonieky' + sob
+}*/
+
+let sobrenome = sob => 'bonieky' + sob
+
+
+console.log(sobrenome('Lacerda'))
